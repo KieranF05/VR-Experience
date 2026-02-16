7c01c388c1b769f61ca82f57ddbbe84f03d707d2
KieranF05 <kieranfotheringham@icloud.com>
2026-02-16 12:43:59 +0000

    fps stuff
    fps stuff

---
+++ Assets/Scripts/HMD_Manager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HMD_Manager : MonoBehaviour
+{
+    [SerializeField] GameObject xrPlayer;
+    [SerializeField] GameObject fpsPlayer;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Using Device:" + XRSettings.loadedDeviceName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
